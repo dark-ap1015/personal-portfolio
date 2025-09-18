2b8a82b86cd0c46fb230dbccd1c65bb6a5cded5c
Aaren Pan <apan1015.cs@gmail.com>
2025-09-18 02:05:41 -0400

    Established site foundation by importing expected sections and features

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import NavBar from './components/NavBar.js';
+import Banner from './components/Banner.js';
+import About from './components/About.js';
+import { Container } from 'react-bootstrap';
+import './assets/css/App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Skills from './components/Skills.js';
+import Experience from './components/Experience.js';
+import Projects from './components/Projects.js';
+import Contact from './components/Contact.js';
+import Footer from './components/Footer.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      <Banner />
+      <section className="body">
+        <Container className="box">
+          <About />
+          <Skills />
+          <Experience />
+          <Projects />
+        </Container>
+      </section>
+      <Contact />
+      <Footer />
     </div>
   );
 }
